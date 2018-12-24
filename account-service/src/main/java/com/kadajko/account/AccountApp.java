a49d8255882fb7b093c4e42b8a27dd42f168ea6b
kadajko <ngockhanhnguyenn@gmail.com>
2018-12-24 10:35:58 +0700

    Update account-service

--- account-service/src/main/java/com/kadajko/account/AccountApp.java
+++ account-service/src/main/java/com/kadajko/account/AccountApp.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
